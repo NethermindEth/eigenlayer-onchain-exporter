87ef4e86bc30b520cd6b6d93985a98fe477f2415
AntiD2ta <mtpotro41@gmail.com>
2024-01-17 12:55:00 +0000

    feat: Serve original requested_at instead of time_difference

--- metrics.py
+++ metrics.py
@@ -13,7 +13,7 @@ def create_metrics():
         'reference_block_number': Gauge('reference_block_number', 'Reference block number'),
         'batch_id': Gauge('batch_id', 'Batch ID'),
         'confirmation_block_number': Gauge('confirmation_block_number', 'Confirmation block number'),
-        'time_difference': Gauge('time_difference_hours', 'Difference in hours from the requested_at time to current time')
+        'requested_at': Gauge('requested_at', 'Time when the blob was requested'),
         # Additional metrics can be added here based on the data fields
     }
     return metrics
@@ -28,13 +28,10 @@ def update_metrics(metrics, data):
     """
     current_time = time.time()
     for blob in data.get('result', {}).get('data', {}).get('json', {}).get('data', []):
-        requested_at = blob.get('requested_at', 0)
-        time_difference = (current_time - requested_at) / 3600  # Convert seconds to hours
-
         metrics['blob_index'].set(blob.get('blob_index', 0))
         metrics['reference_block_number'].set(blob.get('reference_block_number', 0))
         metrics['batch_id'].set(blob.get('batch_id', 0))
         metrics['confirmation_block_number'].set(blob.get('confirmation_block_number', 0))
-        metrics['time_difference'].set(time_difference)
+        metrics['requested_at'].set(blob.get('requested_at', 0)
         # Update additional metrics here
 
