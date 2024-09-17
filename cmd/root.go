42ee4ebf92a370b122ad80ae69f17f8346917afb
Adrian <adriantpaez@gmail.com>
2024-09-17 11:00:02 +0000

    doc: Update root command short description

--- cmd/root.go
+++ cmd/root.go
@@ -7,7 +7,7 @@ import (
 func RootCmd() *cobra.Command {
 	var rootCmd = &cobra.Command{
 		Use:   "eoe",
-		Short: "EigenLayer On-chain Exporter (eoe) exposes Prometheus metrics about EigenLayer's Node Operator.",
+		Short: "EigenLayer On-chain Exporter (eoe) exposes Prometheus metrics about AVS protocols and EigenLayer's Node Operator.",
 	}
 	rootCmd.PersistentFlags().StringP("config", "c", "config.yml", "path to config file")
 	rootCmd.AddCommand(runCommand())
