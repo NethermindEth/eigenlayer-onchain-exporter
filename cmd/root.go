476f35033298a0a4c29e2cc6365bc9a4a8da5e89
Adrian <adriantpaez@gmail.com>
2024-09-18 13:54:54 +0000

    refactor: Update config path default value

--- cmd/root.go
+++ cmd/root.go
@@ -9,7 +9,7 @@ func RootCmd() *cobra.Command {
 		Use:   "eoe",
 		Short: "EigenLayer On-chain Exporter (eoe) exposes Prometheus metrics about AVS protocols and EigenLayer's Node Operator.",
 	}
-	rootCmd.PersistentFlags().StringP("config", "c", "config.yml", "path to config file")
+	rootCmd.PersistentFlags().StringP("config", "c", "eoe-config.yml", "path to config file")
 	rootCmd.AddCommand(runCommand())
 
 	return rootCmd
